a0217c34fa6020026717f47224cf278c0ce7dbbc
jteh89 <j-teh@hotmail.com>
2014-10-12 16:01:52 +1100

    Added JamesScene
    Added test scene and began work on waypoints

---
+++ UnityProject/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 position;
+		Vector3 playerPointA;
+		Vector3 playerPointB;
+		Vector3 enemyPointA;
+		Vector3 enemyPointB;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
