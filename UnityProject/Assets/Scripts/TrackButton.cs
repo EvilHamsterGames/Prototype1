96c004727ade6f8e769fb76126007eb0aa2cccb8
jteh89 <j-teh@hotmail.com>
2014-10-16 20:41:03 +1100

    Started code for track switches

---
+++ UnityProject/Assets/Scripts/TrackButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackButton : MonoBehaviour {
+
+    public Waypoint track;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 60, 40, 120, 25), "Switch Track"))
+        {
+            track.TogglePlayerTrack();
+        }
+    }
+}
