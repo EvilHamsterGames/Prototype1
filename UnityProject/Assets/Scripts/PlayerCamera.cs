0a1255f0e1291de965c5afc575b88a30a0e64328
Liam Parker <parker.liam5@gmail.com>
2014-10-19 21:57:24 +1100

    Started on camera
    - Just added some variables for panning.
    - Fixed some issues with my scene.

--- UnityProject/Assets/Scripts/PlayerCamera.cs
+++ UnityProject/Assets/Scripts/PlayerCamera.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class PlayerCamera : MonoBehaviour {
 
+    public float edgeDistance = 50f;
+    public int panSpeed = 3;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+
 	}
 }
