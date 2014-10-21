1a13abd8aa01064de5fa365c278a04a52a71deba
Angie-H <angie.harvey@students.aie.edu.au>
2014-10-22 01:40:06 +1000

    Menu Buttons
    More Menu Buttons

---
+++ UnityProject/Assets/Scripts/TimeTrialModeButton.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var image: GUITexture;
+var Button: Texture2D;
+var Button_Down: Texture2D;
+
+function OnMouseEnter() {
+
+	image.texture = Button_Down;
+	
+}
+
+function OnMouseExit() {
+
+	image.texture = Button;
+	
+}
+
+function OnMouseDown() {
+
+	Application.LoadLevel("TimeTrial_Mode");
+}
