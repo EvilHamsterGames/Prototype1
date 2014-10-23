65c1913cf95b23a48cc36124d656ebc2eec21afa
Liam Parker <parker.liam5@gmail.com>
2014-10-24 01:24:49 +1100

    AI

---
+++ UnityProject/Assets/Scripts/AI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+    GameManager manager;
+
+    static float timer;
+    public float tickTime = 2;
+
+    //Variable that stores random number dictating AI's choices
+    private int decision;
+
+	// Use this for initialization
+	void Start () 
+    {
+        manager = this.GetComponent("GameManager") as GameManager;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timer += Time.deltaTime;
+        decision = Random.Range(0, 3);
+
+        if(timer < tickTime)
+
+        switch (decision)
+        {
+
+        }
+
+	}
+}
