1f1b11d20cd3a2a4f08679fe51a63089844bdff8
Liam Parker <parker.liam5@gmail.com>
2014-10-24 01:48:50 +1100

    Implemented AI
    - started on AI
    - functioning but dumb as hell

--- UnityProject/Assets/Scripts/AI.cs
+++ UnityProject/Assets/Scripts/AI.cs
@@ -5,8 +5,9 @@ public class AI : MonoBehaviour {
 
     GameManager manager;
 
-    static float timer;
-    public float tickTime = 2;
+    static float timer = 0.0f;
+    public float tickTime = 5.0f;
+    public int randomSeed = 255;
 
     //Variable that stores random number dictating AI's choices
     private int decision;
@@ -15,19 +16,36 @@ public class AI : MonoBehaviour {
 	void Start () 
     {
         manager = this.GetComponent("GameManager") as GameManager;
+        Random.seed = randomSeed;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
         timer += Time.deltaTime;
-        decision = Random.Range(0, 3);
+        decision = Random.Range(-1, 4);
 
-        if(timer < tickTime)
+        if (timer < tickTime)
+            return;
 
         switch (decision)
         {
-
+            case 0:
+                manager.SpawnMinion(GameManager.Participants.ENEMY, Minion.MINIONTYPE.MINIONTYPE_LIGHT);
+                timer = 0.0f;
+                break;
+            case 1:
+                manager.SpawnMinion(GameManager.Participants.ENEMY, Minion.MINIONTYPE.MINIONTYPE_MEDIUM);
+                timer = 0.0f;
+                break;
+            case 2:
+                manager.SpawnMinion(GameManager.Participants.ENEMY, Minion.MINIONTYPE.MINIONTYPE_HEAVY);
+                timer = 0.0f;
+                break;
+            case 3:
+                Debug.Log("AI did nothing this tick");
+                timer = 0.0f;
+                break;
         }
 
 	}
