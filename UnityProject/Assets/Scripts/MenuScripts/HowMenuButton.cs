b04d03076bf16ebb9967a2355003f23494eba8eb
Liam Parker <parker.liam5@gmail.com>
2014-10-23 18:32:45 +1100

    Finished ceonverting buttons to CS

---
+++ UnityProject/Assets/Scripts/MenuScripts/HowMenuButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowMenuButton : MonoBehaviour {
+
+    public GUITexture image;
+    public Texture2D Button;
+    public Texture2D Button_Down;
+
+    public void OnMouseEnter() {
+
+    	image.texture = Button_Down;
+	
+    }
+
+    public void OnMouseExit() {
+
+	    image.texture = Button;
+	
+    }
+
+    public void OnMouseDown()
+    {
+
+	    Application.LoadLevel("HowToPlay");
+    }
+
+}
---
+++ UnityProject/Assets/Scripts/MenuScripts/PlayMenuButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMenuButton : MonoBehaviour {
+
+    public GUITexture image;
+    public Texture2D Button;
+    public Texture2D Button_Down;
+
+    public void OnMouseEnter() {
+
+    	image.texture = Button_Down;
+	
+    }
+
+    public void OnMouseExit() {
+
+    	image.texture = Button;
+	
+    }
+
+    public void OnMouseDown() {
+
+    	Application.LoadLevel("Modes");
+    }
+}
