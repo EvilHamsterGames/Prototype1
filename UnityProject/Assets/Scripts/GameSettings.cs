ac2efe4ba3bdc55d1e4bf69cd4f9dac08841b7a1
Liam Parker <parker.liam5@gmail.com>
2014-10-23 17:42:53 +1100

    Added GameSettings

---
+++ UnityProject/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings : MonoBehaviour {
+
+    public enum Mode
+    {
+        SURVIVAL,
+        TIME_TRIAL
+    };
+
+    private static Mode currentMode;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static void SetMode(Mode newMode)
+    {
+        currentMode = newMode;
+    }
+
+    public static Mode GetMode()
+    {
+        return currentMode;
+    }
+}
