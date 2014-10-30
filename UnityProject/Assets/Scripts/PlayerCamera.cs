23ab149ce7a8b2317f30b84f10cc1641d6335caa
Liam Parker <parker.liam5@gmail.com>
2014-10-30 18:40:53 +1100

    Camera update
    - added bounds
    - changed clear colour to black
    - increased max zoom out

--- UnityProject/Assets/Scripts/PlayerCamera.cs
+++ UnityProject/Assets/Scripts/PlayerCamera.cs
@@ -7,7 +7,7 @@ public class PlayerCamera : MonoBehaviour {
     public float panSpeed = 0.3f;
 
     public float camZoomInMax = 5.0f;
-    public float camZoomOutMax = 15.0f;
+    public float camZoomOutMax = 17.0f;
     public float camZoomSpeed = 0.3f;
 
     private Vector3 tempPos;
@@ -24,16 +24,25 @@ public class PlayerCamera : MonoBehaviour {
         tempPos = transform.position;
 
         if (Input.mousePosition.x <= 0 + edgeDistance || Input.GetKey(KeyCode.LeftArrow))
-            tempPos += new Vector3(-1 * panSpeed, 0, 0);
-
+        {
+            if (camera.transform.position.x > -40)
+                tempPos += new Vector3(-1 * panSpeed, 0, 0);
+        }
         if (Input.mousePosition.x >= Screen.width - edgeDistance || Input.GetKey(KeyCode.RightArrow))
-            tempPos += new Vector3(1 * panSpeed, 0, 0);
-
+        {
+            if (camera.transform.position.x < 40)
+                tempPos += new Vector3(1 * panSpeed, 0, 0);
+        }
         if (Input.mousePosition.y <= 0 + edgeDistance || Input.GetKey(KeyCode.DownArrow))
-            tempPos += new Vector3(0, -1 * panSpeed, 0);
-
+        {
+            if(camera.transform.position.y > -20)
+                tempPos += new Vector3(0, -1 * panSpeed, 0);
+        }
         if (Input.mousePosition.y >= Screen.height - edgeDistance || Input.GetKey(KeyCode.UpArrow))
-            tempPos += new Vector3(0, 1 * panSpeed, 0);
+        {
+            if (camera.transform.position.y < 20)
+                tempPos += new Vector3(0, 1 * panSpeed, 0);
+        }
 
         //Maybe adding zoom functionality here
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
